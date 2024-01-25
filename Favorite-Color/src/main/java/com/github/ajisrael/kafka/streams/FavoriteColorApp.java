887adfc8e12188a95999e9c49402f93aa7dccac7
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-01-25 09:40:36 -0500

    f   - completed favorite color app

--- Favorite-Color/src/main/java/com/github/ajisrael/kafka/streams/FavoriteColorApp.java
+++ Favorite-Color/src/main/java/com/github/ajisrael/kafka/streams/FavoriteColorApp.java
@@ -2,7 +2,9 @@ package com.github.ajisrael.kafka.streams;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
@@ -10,6 +12,7 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -17,28 +20,35 @@ import java.util.Properties;
 public class FavoriteColorApp {
     public Topology createTopology() {
         StreamsBuilder builder = new StreamsBuilder();
-        String[] acceptedColors = {"red", "green", "blue"};
 
-        KStream<String, String> userColors = builder.stream("favorite-color-input");
-        KTable<String, String> filteredUserColors = userColors
-                // standardize input to lowercase
-                .mapValues(userColor -> userColor.toLowerCase())
-                // filter out any not accepted colors
-                .filter((key, value) -> Arrays.asList(acceptedColors).contains(value))
-                // save to a KTable
-                .toTable();
+        // Step 1: We create the topic of users keys to colors
+        KStream<String, String> textLines = builder.stream("favorite-color-input");
 
-        KTable<String, Long> favoriteColors = filteredUserColors
-                // take values and set them as keys
-                .toStream().selectKey((key, value) -> value)
-                // group by colors
-                .groupByKey()
-                // count occurances of colors
-                .count(Materialized.as("Counts"));
+        KStream<String, String> usersAndColors = textLines
+                // 1 - we ensure that a comma is here as we will split on it
+                .filter((key, value) -> value.contains(","))
+                // 2 - we select a key that will be the user id (lowercase for safety)
+                .selectKey((key, value) -> value.split(",")[0].toLowerCase())
+                // 3 - we get the color from the value (lowercase for safety)
+                .mapValues(value -> value.split(",")[1].toLowerCase())
+                // 4 - we filter undesired colors (could be a data sanitization step
+                .filter((user, color) -> Arrays.asList("green", "blue", "red").contains(color));
 
+        usersAndColors.to("user-keys-and-colors");
 
-        // to in order to write the results back to kafka
-        favoriteColors.toStream().to("favorite-color-output", Produced.with(Serdes.String(), Serdes.Long()));
+        // step 2 - we read that topic as a KTable so that updates are read correctly
+        KTable<String, String> usersAndColorsTable = builder.table("user-keys-and-colors");
+
+        // step 3 - we count the occurrences of colors
+        KTable<String, Long> favoriteColors = usersAndColorsTable
+                // 5 - we group by color within the KTable
+                .groupBy((user, color) -> new KeyValue<>(color, color))
+                .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("CountsByColors")
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.Long()));
+
+        // 6 - we output the results to a Kafka Topic - don't forget the serializers
+        favoriteColors.toStream().to("favorite-color-output", Produced.with(Serdes.String(),Serdes.Long()));
 
         return builder.build();
     }
@@ -57,20 +67,14 @@ public class FavoriteColorApp {
         FavoriteColorApp favoriteColorApp = new FavoriteColorApp();
 
         KafkaStreams streams = new KafkaStreams(favoriteColorApp.createTopology(), config);
+        // only for dev - not prod
+        streams.cleanUp();
         streams.start();
+
+        // print the topology
+        streams.localThreadsMetadata().forEach(data -> System.out.println(data));
 
         // shutdown hook to correctly close the streams application
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-
-
-        // print the topology every 5 seconds for learning purposes
-        while(true) {
-            streams.localThreadsMetadata().forEach(data -> System.out.println(data));
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                break;
-            }
-        }
     }
 }
